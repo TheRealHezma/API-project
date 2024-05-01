d90065e1ebbd8a5807c4e9ffd90e25f6b0a4c99a
Hemza <hezmam6@gmail.com>
2024-05-01 19:39:58 -0400

    finishing routes

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -29,6 +29,91 @@ const { User, Spot, Booking, Review, ReviewImage, SpotImage } = require('../../d
 // handleValidationErrors
 //];
 
+// create new spot
+router.post(
+    '/',
+    async (req, res) => {
+
+    }
+);
+
+//Get spots based on id
+router.get('/:spotId', async (req, res) => {
+    try {
+        const { spotId } = req.params;
+        const spot = await Spot.findByPk(spotId,
+            {
+                attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat',
+                    'lng', 'name', 'description', 'price', 'createdAt', 'updatedAt'],
+                include: [
+                    { model: SpotImage, attributes: ['id', 'url', 'preview'] },
+                    { model: User, as: "Owner", attributes: ['id', 'firstName', 'lastName'] }
+                ]
+            });
+
+        const sum = await Review.sum('stars', { where: { spotId: spotId } });
+        const count = await Review.count({ where: { spotId: spotId } });
+        const average = sum / count;
+
+        const spotImages = spot.SpotImages.map(image => {
+            return {
+                id: image.id,
+                url: image.url,
+                preview: image.preview
+            };
+        });
+
+        const owner = {
+            id: spot.Owner.id,
+            firstName: spot.Owner.firstName,
+            lastName: spot.Owner.lastName
+        };
+
+        const response = {
+            id: spot.id,
+            ownerId: spot.ownerId,
+            address: spot.address,
+            city: spot.city,
+            state: spot.state,
+            country: spot.country,
+            lat: spot.lat,
+            lng: spot.lng,
+            name: spot.name,
+            description: spot.description,
+            price: spot.price,
+            createdAt: spot.createdAt,
+            updatedAt: spot.updatedAt,
+            numReviews: count,
+            avgStarRating: average,
+            SpotImages: spotImages,
+            Owner: owner
+        };
+
+        res.status(200);
+        return res.json(response);
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(404).json({ message: "Spot couldn't be found" });
+    }
+});
+
+// Get spots of current user
+router.get('/current', async (req, res) => {
+    try {
+        const userId = req.user.id;
+        const spots = await Spot.findAll({ where: { ownerId: userId } });
+
+        if (spots.length > 0) {
+            res.json({ spots });
+        } else {
+            res.json({ message: 'No spots found for the logged-in user' });
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
 
 // GET all spots
 router.get('/', async (req, res) => {
@@ -47,7 +132,21 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: 'Internal server error' });
     }
 });
+//Get spots of current user
+// router.get('//current', async (req, res) => {
+//     try {
+//         const userId = req.user.id;
+//         const spots = await Spot.findAll({ where: { ownerId: userId } });
 
-
+//         if (spots.length > 0) {
+//           res.json({ spots });
+//         } else {
+//           res.json({ message: 'No spots found for the logged-in user' });
+//         }
+//       } catch (error) {
+//         console.error('Error:', error);
+//         res.status(500).json({ error: 'Internal server error' });
+//       }
+// })
 
 module.exports = router;
