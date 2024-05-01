57348614d236ceb6d18f68f1c5c42263a82e6f1c
Hemza <hezmam6@gmail.com>
2024-05-01 12:09:36 -0400

    working on spot route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,8 +1,13 @@
-// backend/routes/api/index.js
 const router = require('express').Router();
 const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
-const { restoreUser } = require("../../utils/auth.js");
+const spotsRouter = require('./spots.js');
+// const spotsImagesRouter = require('./spot-images.js');
+// const reviewsRouter = require('./reviews.js');
+// const reviewsImagesRouter = require('./review-images.js');
+// const bookingsRouter = require('./bookings.js')
+
+const { restoreUser } = require('../../utils/auth.js');
 
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
@@ -12,6 +17,16 @@ router.use(restoreUser);
 router.use('/session', sessionRouter);
 
 router.use('/users', usersRouter);
+
+router.use('/spots', spotsRouter);
+
+// router.use('/spot-images', spotsImagesRouter);
+
+// router.use('/reviews', reviewsRouter);
+
+// router.use('/review-images', reviewsImagesRouter);
+
+// router.use('/bookings', bookingsRouter);
 
 router.post('/test', (req, res) => {
     res.json({ requestBody: req.body });
