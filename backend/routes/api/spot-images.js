109b50035f6aa98030cc6c29b974ab90c39696b2
Hemza <hezmam6@gmail.com>
2024-05-03 18:54:17 -0400

    delete spot-img

--- backend/routes/api/spot-images.js
+++ backend/routes/api/spot-images.js
@@ -7,39 +7,52 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const { validationResult } = require('express-validator');
 
-const validateSpotCreation = [
-    check('address')
-        .exists()
-        .withMessage('Street address is required'),
-    check('city')
-        .exists()
-        .withMessage('City is required'),
-    check('state')
-        .exists()
-        .withMessage('State is required'),
-    check('country')
-        .exists()
-        .withMessage('Country is required'),
-    check('lat')
-        .exists()
-        .withMessage('Latitude is not valid'),
-    check('lng')
-        .exists()
-        .withMessage('Longitude is not valid'),
-    check('name')
-        .exists()
-        .withMessage('Name is required')
-        .isLength({ max: 50 })
-        .withMessage('Name must be less than 50 characters'),
-    check('description')
-        .exists()
-        .withMessage('Description is required'),
-    check('price')
-        .exists()
-        .withMessage('Price per day is required'),
-    handleValidationErrors
-];
+// const validateSpotCreation = [
+//     check('address')
+//         .exists()
+//         .withMessage('Street address is required'),
+//     check('city')
+//         .exists()
+//         .withMessage('City is required'),
+//     check('state')
+//         .exists()
+//         .withMessage('State is required'),
+//     check('country')
+//         .exists()
+//         .withMessage('Country is required'),
+//     check('lat')
+//         .exists()
+//         .withMessage('Latitude is not valid'),
+//     check('lng')
+//         .exists()
+//         .withMessage('Longitude is not valid'),
+//     check('name')
+//         .exists()
+//         .withMessage('Name is required')
+//         .isLength({ max: 50 })
+//         .withMessage('Name must be less than 50 characters'),
+//     check('description')
+//         .exists()
+//         .withMessage('Description is required'),
+//     check('price')
+//         .exists()
+//         .withMessage('Price per day is required'),
+//     handleValidationErrors
+// ];
 
+// Route for deleting a spot image by its ID
+router.delete('/:imageId', requireAuth, async (req, res, next) => {
+    const imageId = req.params.imageId;
+    const spotImage = await SpotImage.findByPk(imageId);
+
+    if (!spotImage) {
+        return res.status(404).json({ message: "Spot image not found" });
+    }
+
+    await spotImage.destroy();
+
+    return res.status(200).json({ message: "Spot image deleted successfully" });
+});
 
 
 module.exports = router;
