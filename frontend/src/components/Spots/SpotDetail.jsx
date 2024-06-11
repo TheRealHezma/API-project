b57cd6d6f35e38df5e73c6248982f4adf137457d
Hemza <hezmam6@gmail.com>
2024-06-10 20:42:59 -0400

    little edits

--- frontend/src/components/Spots/SpotDetail.jsx
+++ frontend/src/components/Spots/SpotDetail.jsx
@@ -1,7 +1,5 @@
 //frontend/src/components/Spots/SpotDetail.jsx
 
-//frontend/src/components/Spots/SpotDetail.jsx
-
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
